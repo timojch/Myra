6a7e2a1f2a0053cec5d808e2eaf7238428b14bdb
timojch <timo.ch@outlook.com>
2025-07-04 14:54:43 -0700

    More flow container stuff.

---
+++ src/Myra/Graphics2D/UI/Containers/FlowContainer.cs
@@ -0,0 +1,155 @@
+﻿using Myra.Attributes;
+using Myra.Graphics2D.UI.Layouts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Myra.Graphics2D.UI.Containers
+{
+    public class FlowContainer : Container
+    {
+        private readonly ObservableCollection<Widget> _widgets = new ObservableCollection<Widget>();
+        private readonly FlowLayout _layout = new FlowLayout();
+        private bool _childrenDirty = false;
+
+        [Content]
+        [Browsable(false)]
+        public override ObservableCollection<Widget> Widgets => _widgets;
+
+        public List<ControlPoint> ControlPoints { get => this._layout.ControlPoints; }
+
+        public FlowContainer()
+        {
+            this._widgets.CollectionChanged += OnWidgetsChanged;
+            this.ChildrenLayout = _layout;
+        }
+
+        public ControlPoint AddLineBreak(int indent = 0)
+        {
+            if (this.Widgets.Count > 0)
+            {
+                return this.AddLineBreakAfter(this.Widgets.Last(), indent);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public ControlPoint AddLineBreakAfter(Widget widget, int indent = 0)
+        {
+            var cp = new ControlPoint
+            {
+                AnchorWidget = widget,
+                Indent = indent,
+                LineBreak = true
+            };
+            this.ControlPoints.Add(cp);
+            return cp;
+        }
+
+        private void OnWidgetsChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Reset)
+            {
+                this.ControlPoints.Clear();
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                this.OnWidgetsRemoved(e.OldStartingIndex, e.OldItems);
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Replace)
+            {
+                if (e.OldItems.Count == e.NewItems.Count)
+                {
+                    for (int i = 0; i < e.OldItems.Count; ++i)
+                    {
+                        var oldAnchor = e.OldItems[i];
+                        var newAnchor = e.NewItems[i];
+
+                        foreach (var cp in this.ControlPoints.Where(cp => cp.AnchorWidget == oldAnchor))
+                        {
+                            cp.AnchorWidget = (Widget)newAnchor;
+                        }
+                    }
+                }
+                else
+                {
+                    this.OnWidgetsRemoved(e.OldStartingIndex, e.OldItems);
+                }
+            }
+
+            _childrenDirty = true;
+            this.InvalidateMeasure();
+        }
+
+        protected override void InternalArrange()
+        {
+            if (this._childrenDirty)
+            {
+                this.Children.Clear();
+                foreach (var widget in this.Widgets)
+                {
+                    this.Children.Add(widget);
+                }
+
+                this._childrenDirty = false;
+            }
+
+            base.InternalArrange();
+        }
+
+        private void OnWidgetsRemoved(int startIndex, IList removedItems)
+        {
+            var affectedPoints = this.ControlPoints.Where(cp => removedItems.Contains(cp.AnchorWidget)).ToArray();
+            var pointsToRemove = affectedPoints.Where(cp => !cp.Persist);
+            var pointsToMoveForward = affectedPoints.Where(cp => cp.Persist && cp.IsBeforeAnchor);
+            var pointsToMoveBackward = affectedPoints.Where(cp => cp.Persist && !cp.IsBeforeAnchor);
+
+            int backwardPoint = startIndex - 1;
+            int forwardPoint = startIndex;
+
+            foreach (var cp in pointsToRemove)
+            {
+                this.ControlPoints.Remove(cp);
+            }
+
+            if (backwardPoint >= 0)
+            {
+                foreach (var cp in pointsToMoveBackward)
+                {
+                    cp.AnchorWidget = this.Widgets[backwardPoint];
+                }
+            }
+
+            if (forwardPoint < this.Widgets.Count)
+            {
+                foreach (var cp in pointsToMoveForward)
+                {
+                    cp.AnchorWidget = this.Widgets[forwardPoint];
+                }
+            }
+
+        }
+
+        public class ControlPoint
+        {
+            public Widget AnchorWidget;
+
+            public bool IsBeforeAnchor;
+
+            public bool LineBreak;
+
+            public bool Persist;
+
+            public int Indent;
+
+            public bool SkipHorizontalSpacing;
+        }
+    }
+}
---
+++ src/Myra/Graphics2D/UI/Layouts/FlowLayout.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Xna.Framework;
+using Myra.Graphics2D.UI.Containers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Myra.Graphics2D.UI.Layouts
+{
+    public class FlowLayout : ILayout
+    {
+        public List<FlowContainer.ControlPoint> ControlPoints { get; } = new List<FlowContainer.ControlPoint>();
+
+        public int IndentSize = 80;
+
+        public int LineSpacing = 4;
+
+        public int HorizontalSpacing = 4;
+
+        public int MinLineHeight = 0;
+
+        public int MaxLineHeight = int.MaxValue;
+
+        public IEnumerable<Tuple<Widget, Rectangle>> GetArrangedRectangles(IEnumerable<Widget> widgets, Point availableSize)
+        {
+            var cursorPos = Point.Zero;
+            var lineStart = Point.Zero;
+            int currentLineHeight = this.MinLineHeight;
+            int currentIndent = 0;
+            int itemsOnCurrentLine = 0;
+            bool isHorizontalSpaceSkipped = true;
+
+            Action funcNewLine = () =>
+            {
+                isHorizontalSpaceSkipped = true;
+                lineStart = lineStart + new Point(0, currentLineHeight + this.LineSpacing);
+                cursorPos = lineStart + new Point(this.IndentSize * currentIndent, 0);
+                currentLineHeight = this.MinLineHeight;
+                itemsOnCurrentLine = 0;
+            };
+
+            Action<FlowContainer.ControlPoint> funcHandleControlPoint = (FlowContainer.ControlPoint cp) =>
+            {
+                currentIndent += cp.Indent;
+                if (cp.LineBreak)
+                {
+                    funcNewLine();
+                }
+
+                if (cp.SkipHorizontalSpacing && !isHorizontalSpaceSkipped)
+                {
+                    cursorPos += new Point(-this.HorizontalSpacing, 0);
+                    isHorizontalSpaceSkipped = true;
+                }
+            };
+
+            foreach (var widget in widgets)
+            {
+                foreach (var cp in this.ControlPoints.Where(cp => cp.AnchorWidget == widget && cp.IsBeforeAnchor))
+                {
+                    funcHandleControlPoint(cp);
+                }
+
+                var measure = widget.Measure(new Point(availableSize.X - cursorPos.X, MaxLineHeight));
+
+                if (cursorPos.X + measure.X > availableSize.X)
+                {
+                    // Doesn't fit. If we aren't already at the start of a new line, move to a new line.
+                    if (itemsOnCurrentLine > 0)
+                    {
+                        funcNewLine();
+                    }
+                }
+
+                yield return new Tuple<Widget, Rectangle>(widget, new Rectangle(cursorPos, measure));
+                cursorPos += new Point(measure.X + this.HorizontalSpacing, 0);
+                isHorizontalSpaceSkipped = false;
+                currentLineHeight = Math.Min(Math.Max(currentLineHeight, measure.Y), this.MaxLineHeight);
+                itemsOnCurrentLine++;
+
+                foreach (var cp in this.ControlPoints.Where(cp => cp.AnchorWidget == widget && !cp.IsBeforeAnchor))
+                {
+                    funcHandleControlPoint(cp);
+                }
+            }
+        }
+
+        public void Arrange(IEnumerable<Widget> widgets, Rectangle bounds)
+        {
+            foreach (var pair in this.GetArrangedRectangles(widgets, bounds.Size))
+            {
+                pair.Item1.Arrange(new Rectangle(pair.Item2.Location + bounds.Location, pair.Item2.Size));
+            }
+        }
+
+        public Point Measure(IEnumerable<Widget> widgets, Point availableSize)
+        {
+            int width = 0, height = 0;
+            foreach (var pair in this.GetArrangedRectangles(widgets, availableSize))
+            {
+                var rect = pair.Item2;
+                width = Math.Max(width, rect.Right);
+                height = Math.Max(height, rect.Bottom);
+            }
+
+            return new Point(width, height);
+        }
+    }
+}
