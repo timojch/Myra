b5845fa7e4c0a58078ff83d63661939a9ffd92eb
timojch <timo.ch@outlook.com>
2025-07-05 16:36:32 -0700

    Added single content panel widget.

---
+++ src/Myra/Graphics2D/UI/Simple/ContentPanel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myra.Graphics2D.UI
+{
+    public class ContentPanel : ContentControl
+    {
+        private SingleItemLayout<Widget> _layout;
+
+        public override Widget Content
+        {
+            get => this._layout.Child;
+            set => this._layout.Child = value;
+        }
+
+        public ContentPanel()
+        {
+            this._layout = new SingleItemLayout<Widget>(this);
+            this.ChildrenLayout = this._layout;
+        }
+    }
+}
