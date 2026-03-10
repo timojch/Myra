dfd145e0f4e9921e4e4fbdb898147eb435ed3454
timojch <timo.ch@outlook.com>
2026-03-10 00:33:38 -0700

    Added layout which is like SingleItemLayout, but which can have other children, which are manually arranged.

---
+++ src/Myra/Graphics2D/UI/Layouts/PrimaryItemLayout.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Myra.Utility;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Myra.Graphics2D.UI;
+
+public class PrimaryItemLayout<T> : ILayout where T : Widget
+{
+    private readonly Widget _container;
+
+    private ObservableCollection<Widget> Children => _container.Children;
+
+    public T Child
+    {
+        get;
+        set
+        {
+            if (field is not null && Children.Contains(field))
+            {
+                var childPosition = Children.IndexOf(field);
+                Children[childPosition] = value;
+            }
+            else
+            {
+                Children.Add(value);
+            }
+
+            field = value;
+        }
+    }
+
+    public PrimaryItemLayout(Widget container)
+    {
+        _container = container ?? throw new ArgumentNullException(nameof(container));
+    }
+
+    public Point Measure(IEnumerable<Widget> widgets, Point availableSize)
+    {
+        var result = Mathematics.PointZero;
+
+        if (Child != null)
+        {
+            result = Child.Measure(availableSize);
+        }
+
+        return result;
+    }
+
+    public void Arrange(IEnumerable<Widget> widgets, Rectangle bounds)
+    {
+        if (Child != null && Child.Visible)
+        {
+            Child.Arrange(bounds);
+        }
+    }
+}
+}
