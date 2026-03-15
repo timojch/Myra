303aec09cb3a00fc31888c8b12dd016a2abdb8b4
timojch <timo.ch@outlook.com>
2026-03-14 19:10:56 -0700

    Added overlappedlayout for when you want a SingleItemLayout behavior for multiple widgets simultaneously.

---
+++ src/Myra/Graphics2D/UI/Layouts/OverlappedLayout.cs
@@ -0,0 +1,55 @@
+﻿using Myra.Utility;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System;
+
+#if MONOGAME || FNA
+using Microsoft.Xna.Framework;
+#elif STRIDE
+using Stride.Core.Mathematics;
+#else
+using System.Drawing;
+#endif
+
+namespace Myra.Graphics2D.UI;
+
+public class OverlappedLayout : ILayout
+{
+    private readonly Widget _container;
+
+    private ObservableCollection<Widget> Children => _container.Children;
+
+    public OverlappedLayout(Widget container)
+    {
+        _container = container ?? throw new ArgumentNullException(nameof(container));
+    }
+
+    public Point Measure(IEnumerable<Widget> widgets, Point availableSize)
+    {
+        var result = Mathematics.PointZero;
+
+        foreach (var child in this.Children)
+        {
+            if (child.Visible)
+            {
+                var measure = child.Measure(availableSize);
+                var x = Math.Max(result.X, measure.X);
+                var y = Math.Max(result.Y, measure.Y);
+                result = new Point(x, y);
+            }
+        }
+
+        return result;
+    }
+
+    public void Arrange(IEnumerable<Widget> widgets, Rectangle bounds)
+    {
+        foreach (var child in this.Children)
+        {
+            if (child.Visible)
+            {
+                child.Arrange(bounds);
+            }
+        }
+    }
+}
